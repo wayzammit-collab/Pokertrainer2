43085751036bb9e7758a9754a8049d38a5890c52
wayzammit-collab <way.zammit@gmail.com>
2025-09-16 11:20:47 +0200

    updated vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 export default defineConfig({
-base: "/Pokertrainer2/", // add this line
+base: "/Pokertrainer2/",
 plugins: [react()],
 server: { port: 5173, host: true },
 });
