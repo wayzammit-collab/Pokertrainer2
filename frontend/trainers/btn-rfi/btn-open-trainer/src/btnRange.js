d0124efa4f862c4ce7bd79f95fc8361132f572da
wayzammit-collab <way.zammit@gmail.com>
2025-09-17 10:18:42 +0200

    V4

---
+++ frontend/trainers/btn-rfi/btn-open-trainer/src/btnRange.js
@@ -0,0 +1,15 @@
+const BTN_OPEN_100BB = {
+  "AA":true, "KK":true, "QQ":true, "JJ":true, "TT":true, "99":true, "88":true, "77":true, "66":true, "55":true, "44":true, "33":true, "22":true,
+  "AKs":true, "AQs":true, "AJs":true, "ATs":true, "A9s":true, "A8s":true, "A7s":true, "A6s":true,
+  "A5s":true, "A4s":true, "A3s":true, "A2s":true,
+  "KQs":true, "KJs":true, "KTs":true, "K9s":true,
+  "QJs":true, "QTs":true, "Q9s":true,
+  "JTs":true, "J9s":true, "T9s":true,
+  "98s":true, "87s":true, "76s":true, "65s":true, "54s":true,
+  "T8s":true, "97s":true, "86s":true, "75s":true, "64s":true, "53s":true,
+  "AKo":true, "AQo":true, "AJo":true, "ATo":true,
+  "KQo":true, "KJo":true, "KTo":true,
+  "QJo":true, "QTo":true,
+  "JTo":true
+};
+export default BTN_OPEN_100BB;
